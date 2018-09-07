09ac23fee828e4c9e212dd109782981750b4e417
Pieter <pb.marcus@hotmail.com>
2018-09-08 01:17:57 +0200

    adjusting theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,9 +32,14 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode']
+
+napoleon_google_docstring = False
+napoleon_use_param = False
+napoleon_use_ivar = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['ntemplates']
